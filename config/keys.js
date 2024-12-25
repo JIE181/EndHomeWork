b1ac58e8a3621492f47a96f8f8d5566300970dbe
huangjie <3530855346@qq.com>
2024-12-25 16:29:22 +0800

    first commit

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI: "mongodb+srv://2290616675:13142350127@node-vue-element.ii6cs.mongodb.net/?retryWrites=true&w=majority&appName=node-vue-element",
+  secretOrKey: "secret",
+};
